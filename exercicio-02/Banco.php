c63e5cf24bc9ef9efb373c34b091341a1a368548
Matheus Caetano <caetanomatheus876@gmail.com>
2020-01-02 08:19:34 -0300

    Mudanças nos métodos de criação de conta

--- exercicio-02/Banco.php
+++ exercicio-02/Banco.php
@@ -3,30 +3,27 @@ class Banco {
 
     private $contas = array();
 
-    public function fecharConta($conta) {
-        for ($i = 0; $i < count($this->contas); $i++) { 
-            if ($this->contas[$i]->getNumero() == $conta->getNumero()) {
+    public function fecharConta($contaCorrente) {
+        for ($i = 0; $i < count($this->contas); $i++) {
+            if ($this->contas[$i]->getNumero() == $contaCorrente->getNumero()) {
                 unset($this->contas[$i]);
-                return true;   
-            }
-        }
-        return false;
-    }
-
-    public function abrirConta($conta) {
-        if (!$this->contaExiste($conta->getNumero())) {
-            array_push($this->contas, $conta);
-            return true;
-        } else return false;
-    }
-
-    private function contaExiste($numero) {
-        foreach ($this->contas as $conta) {
-            if ($conta->getNumero() == $numero) {
                 return true;
             }
         }
         return false;
+    }
+
+    public function abrirConta($contaCorrente) {
+        if ($this->contaExiste($contaCorrente)) return false;
+        array_push($this->contas, $contaCorrente);
+        return true;
+    }
+
+    public function contaExiste($contaCorrente) {
+        foreach ($this->contas as $conta) {
+            if ($conta->getNumero() == $contaCorrente->getNumero()) return $conta;
+        }
+        return null;
     }
 
     public function getContas() { return $this->contas; }
