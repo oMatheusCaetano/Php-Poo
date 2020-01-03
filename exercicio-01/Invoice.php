5e88ae04f35af10edf9253b4a9e50c8cc666987a
Matheus Caetano <caetanomatheus876@gmail.com>
2020-01-03 12:55:13 -0300

    Resolução do Exercício 01

---
+++ exercicio-01/Invoice.php
@@ -0,0 +1,35 @@
+<?php
+class Invoice {
+
+    private $code;
+    private $description;
+    private $quantity;
+    private $price;
+
+    public function Invoice($code, $price, $quantity, $description = "NO DESCRIPTION") {
+        $this->setCode($code);
+        $this->setDescription($description);
+        $this->setQuantity($quantity);
+        $this->setPrice($price);
+    }
+
+    public function getInvoiceAmount() {
+        return $this->getQuantity() * $this->getPrice();
+    }
+
+    public function getCode() { return $this->code; }
+
+    public function setCode($code) { $this->code = $code; }
+
+    public function getDescription() { return $this->description; }
+
+    public function setDescription($description) { $this->description = $description; }
+
+    public function getQuantity() { return $this->quantity; }
+
+    public function setQuantity($quantity) { $this->quantity = $quantity < 0 ? 0 : $quantity; }
+
+    public function getPrice() { return $this->price; }
+
+    public function setPrice($price) { $this->price = $price < 0 ? 0 : $price; }
+}
--- index.php
+++ index.php
@@ -1,18 +1,12 @@
 <title>PHP-POO</title>
 <pre>
 <?php
-require_once("exercicio-02/Banco.php");
-require_once("exercicio-02/ContaCorrente.php");
-$banco = new Banco();
-$conta = new ContaCorrente(1, 20000, 2500);
-$conta2 = new ContaCorrente(2, 15000, 3000);
-$conta3 = new ContaCorrente(3, 30000, 10000);
-$banco->abrirConta($conta);
-$banco->abrirConta($conta2);
-$banco->abrirConta($conta3);
-$banco->fecharConta($conta);
-//$banco->fecharConta($conta2);
-//$banco->fecharConta($conta3);
-print_r($banco);
+
+require_once("./exercicio-01/Invoice.php");
+
+$invoice = new Invoice(1, 20, 10, "Product Description");
+print($invoice->getInvoiceAmount() . "<br>");
+print_r($invoice);
+
 ?>
 </pre>
