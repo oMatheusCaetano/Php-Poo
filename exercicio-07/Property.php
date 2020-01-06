c5493b028c50115c244094334a5484281c5d4902
Matheus Caetano <caetanomatheus876@gmail.com>
2020-01-06 07:24:27 -0300

    Continuando exercício 07

--- exercicio-07/Property.php
+++ exercicio-07/Property.php
@@ -1,8 +1,23 @@
 <?php
 class Property {
 
-    private $color;
-    private $doors = array();
-    private $floors;
-    
+    protected $color;
+    protected $doors = array();
+    protected $floors;
+
+    public function quantityOfDoors() { return count($this->doors; )}
+
+    public function addFloor() { $this->floors++; }
+
+    public function getColor() { return $this->color; }   
+
+    public function setColor($color) { $this->color = $color; } 
+
+    public function getDoors() { return $this->doors; }
+
+    public function setDoors($doors) { $this->doors = $doors; }
+
+    public function getFloors() { return $this->floors; }
+
+    public function setFloors($floors) { $this->floors = $floors; }
 }
